d37112a009df93701f23d7ef4a7140e281a02309
Kevin Langat <langatkaykay@gmail.com>
2025-08-29 13:25:55 +0300

    Changes the database to SQLite

--- settings.py
+++ settings.py
@@ -82,14 +82,12 @@ WSGI_APPLICATION = 'receipt_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DRTDB',
-        'USER': 'root',
-        'PASSWORD': '30042004',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

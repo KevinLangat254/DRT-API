13cdebab766714039caf9709f139060478ca1e75
Kevin Langat <langatkaykay@gmail.com>
2026-01-23 16:08:02 +0300

    Deleted some duplicate files

--- receipt_tracker/settings.py
+++ receipt_tracker/settings.py
@@ -11,29 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
-import os
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-
-
-DEBUG = os.getenv("DEBUG", "False").lower() in ("1", "true", "yes")
-
-ALLOWED_HOSTS = ['Kevinpy.pythonanywhere.com', '127.0.0.1', 'localhost']
-
-CSRF_TRUSTED_ORIGINS = ['https://Kevinpy.pythonanywhere.com', 'http://127.0.0.1:8000', 'http://127.0.0.1:8001', 'http://localhost:8000', 'http://localhost:8001']
-
-
-# Application definition
+SECRET_KEY = config("SECRET_KEY", default="dev-secret")
+DEBUG = config("DEBUG", default=True, cast=bool)
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -46,7 +31,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-
 ]
 
 MIDDLEWARE = [
@@ -78,67 +62,39 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'receipt_tracker.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# -----------------------------
+# MySQL Database Configuration
+# -----------------------------
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),       # must be 'django.db.backends.mysql' in .env
+        'NAME': config('DB_NAME'),           # MySQL database name
+        'USER': config('DB_USER'),           # MySQL username
+        'PASSWORD': config('DB_PASSWORD'),   # MySQL password
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
-
-
 # Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST Framework settings
-# This section configures the Django REST Framework to use token authentication
-# and sets the default permission classes to allow only authenticated users.
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
@@ -152,9 +108,6 @@ REST_FRAMEWORK = {
     ],
 }
 
-
-LOGIN_REDIRECT_URL = 'index'          
+LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = '/drt/login/'
-LOGOUT_REDIRECT_URL = '/drt/login/'   
-
-
+LOGOUT_REDIRECT_URL = '/drt/login/'
